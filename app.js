76d4734a3c9c28e336482cad11a27ab46b9971d9
Angie Tran <angietran@gmail.com>
2015-03-23 21:35:54 -0600

    Created a function to prevent repeated code

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 var main = function(){
 	"use strict";
 
-	$(".comment-input button").on("click", function (event) {
-		//Add a comment to the comments section
-		if($(".comment-input input").val() !==""){
-			var $newComment = $("<p>").text($(".comment-input input").val());
-			$(".comments").append($newComment);
+	var addCommentFromInputBox = function(){
+		var $newComment;
 
-			//Reset input box
+		if($(".comment-input input").val() !==""){
+			$newComment = $("<p>").text($(".comment-input input").val());
+			$newComment.hide();
+			$(".comments").append($newComment);
+			$newComment.fadeIn();
+			//	
 			$(".comment-input input").val("");
+
 		}
+	};
+
+	//Update feed when user clicks on "+" button
+	$(".comment-input button").on("click", function (event) {
+		addCommentFromInputBox();
 	});
 
+	//Update feed upon ENTER key press
 	$(".comment-input input").on("keypress", function (event){
 		if(event.keyCode === 13){
-			if($(".comment-input input").val() !==""){
-				var $newComment = $("<p>").text($(".comment-input input").val());
-				$(".comments").append($newComment);
-
-				//Reset input box
-				$(".comment-input input").val("");
-			}
+			addCommentFromInputBox();
 		}
 	});
 };
